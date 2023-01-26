9d9255cd44d3ba6d6cefafa7eae8f0e428cf2a4a
nadiannis <annisanadianeyla@gmail.com>
2023-01-26 09:03:37 +0700

    chore: add custom grid template columns style in tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(240px, 1fr))',
+      },
+    },
   },
   plugins: [],
 };
