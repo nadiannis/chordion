e59b9c2bc02768668f72c26622fb5115d0ed10d9
nadiannis <annisanadianeyla@gmail.com>
2023-02-04 11:43:22 +0700

    feat: add optional handleClick function in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({ children }) {
+export default function Button({ children, handleClick = null }) {
   return (
-    <button className="px-4 py-5 rounded-3xl font-semibold text-xl text-indigo-500 bg-indigo-100 hover:bg-indigo-200 active:bg-indigo-200">
+    <button
+      className="px-4 py-5 rounded-3xl font-semibold text-xl text-indigo-500 bg-indigo-100 hover:bg-indigo-200 active:bg-indigo-200"
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
@@ -11,4 +14,5 @@ export default function Button({ children }) {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  handleClick: PropTypes.func,
 };
