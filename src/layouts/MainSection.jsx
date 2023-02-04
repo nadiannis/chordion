bf468fa47fdb5179e7ee8299a3be58315822afd2
nadiannis <annisanadianeyla@gmail.com>
2023-02-04 10:40:28 +0700

    feat: make the start note options & octave options dynamic

--- src/layouts/MainSection.jsx
+++ src/layouts/MainSection.jsx
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Container from '../components/Container';
 
+const startNotes = [
+  'C',
+  'C#',
+  'Db',
+  'D',
+  'D#',
+  'Eb',
+  'E',
+  'F',
+  'F#',
+  'Gb',
+  'G',
+  'G#',
+  'Ab',
+  'A',
+  'A#',
+  'Bb',
+  'B',
+];
+const octaves = [1, 2, 3, 4, 5, 6, 7];
+
 export default function MainSection() {
+  const [startNote, setStartNote] = useState(startNotes[0]);
+  const [octave, setOctave] = useState(octaves[0]);
+
+  const handleStartNoteChange = (e) => {
+    console.log(e.target.value);
+    setStartNote(e.target.value);
+  };
+
+  const handleOctaveChange = (e) => {
+    console.log(e.target.value);
+    setOctave(e.target.value);
+  };
+
   return (
     <main className="bg-slate-100 bg-gradient-to-tl from-indigo-200 to-cyan-50 h-screen lg:flex-1 lg:sticky lg:top-0 py-7">
       <Container className="flex flex-col h-full">
         <div className="font-semibold mb-8">
-          <span className="block text-lg">Start note: C</span>
-          <span className="block text-lg">Octave: 5</span>
+          <span className="block text-lg">Start note: {startNote}</span>
+          <span className="block text-lg">Octave: {octave}</span>
         </div>
         <div className="font-bold text-[2.6rem] text-indigo-500 mb-8 lg:flex-1 flex justify-center items-center gap-4">
-          <select name="start-note" className="px-2 rounded-lg">
-            <option value="C">C</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
+          <select
+            name="start-note"
+            className="px-2 rounded-lg"
+            onChange={handleStartNoteChange}
+            value={startNote}
+          >
+            {startNotes.map((startNote) => (
+              <option key={startNote} value={startNote}>
+                {startNote}
+              </option>
+            ))}
           </select>
-          <select name="octave" className="px-2 rounded-lg">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
+          <select
+            name="octave"
+            className="px-2 rounded-lg"
+            onChange={handleOctaveChange}
+            value={octave}
+          >
+            {octaves.map((octave) => (
+              <option key={octave} value={octave}>
+                {octave}
+              </option>
+            ))}
           </select>
         </div>
         <div className="bg-white rounded-3xl flex justify-center items-center w-full h-64 lg:h-1/2 shadow lg:shadow-xl px-6 py-12 lg:mt-auto">
