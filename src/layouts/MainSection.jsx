8b0e2e86c2458655bc5d01e2a11054f9e65ebf51
nadiannis <annisanadianeyla@gmail.com>
2023-02-04 12:05:55 +0700

    refactor: change name of start note & octave state

--- src/layouts/MainSection.jsx
+++ src/layouts/MainSection.jsx
@@ -28,33 +28,34 @@ const octaves = [1, 2, 3, 4, 5, 6, 7];
 export default function MainSection() {
   const { selectedChordName } = useChordName();
 
-  const [startNote, setStartNote] = useState(startNotes[0]);
-  const [octave, setOctave] = useState(octaves[0]);
-  const chordIntervals = getChord(selectedChordName).intervals.join(' - ');
+  const [selectedStartNote, setSelectedStartNote] = useState(startNotes[0]);
+  const [selectedOctave, setSelectedOctave] = useState(octaves[0]);
+
+  const chordIntervals = getChord(selectedChordName).intervals;
 
   const handleStartNoteChange = (e) => {
     console.log(e.target.value);
-    setStartNote(e.target.value);
+    setSelectedStartNote(e.target.value);
   };
 
   const handleOctaveChange = (e) => {
     console.log(e.target.value);
-    setOctave(e.target.value);
+    setSelectedOctave(e.target.value);
   };
 
   return (
     <main className="bg-slate-100 bg-gradient-to-tl from-indigo-200 to-cyan-50 h-screen lg:flex-1 lg:sticky lg:top-0 py-7">
       <Container className="flex flex-col h-full">
         <div className="font-semibold mb-8">
-          <span className="block text-lg">Start note: {startNote}</span>
-          <span className="block text-lg">Octave: {octave}</span>
+          <span className="block text-lg">Start note: {selectedStartNote}</span>
+          <span className="block text-lg">Octave: {selectedOctave}</span>
         </div>
         <div className="font-bold text-[2.6rem] text-indigo-500 mb-8 lg:flex-1 flex justify-center items-center gap-4">
           <select
             name="start-note"
             className="px-2 rounded-lg"
             onChange={handleStartNoteChange}
-            value={startNote}
+            value={selectedStartNote}
           >
             {startNotes.map((startNote) => (
               <option key={startNote} value={startNote}>
@@ -66,7 +67,7 @@ export default function MainSection() {
             name="octave"
             className="px-2 rounded-lg"
             onChange={handleOctaveChange}
-            value={octave}
+            value={selectedOctave}
           >
             {octaves.map((octave) => (
               <option key={octave} value={octave}>
@@ -81,7 +82,7 @@ export default function MainSection() {
               C - E - G
             </h2>
             <span className="font-bold text-slate-400 text-4xl xl:text-[2.6rem]">
-              {chordIntervals}
+              {chordIntervals.join(' - ')}
             </span>
           </div>
         </div>
