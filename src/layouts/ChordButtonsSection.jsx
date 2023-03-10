45aad2ec417b0b4ae8ce01721bb0eae987b19373
nadiannis <annisanadianeyla@gmail.com>
2023-03-10 17:37:59 +0700

    refactor: modify ChordButtonsSection

--- src/layouts/ChordButtonsSection.jsx
+++ src/layouts/ChordButtonsSection.jsx
@@ -1,32 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import Sheet from 'react-modal-sheet';
 
 import ChordList from '../components/ChordList';
 import Container from '../components/Container';
 
-const lgBreakpoint = 1024;
 const snapPoints = [0.5, 0.35, 0.2];
-const initialSnapIndex = 2;
-const springConfig = { stiffness: 150, damping: 30, mass: 0.7 };
+const initialSnapIndex = snapPoints.length - 1;
+const springConfig = { stiffness: 150, damping: 20, mass: 0.7 };
 
 export default function ChordButtonsSection() {
   const sheetRef = useRef();
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    window.addEventListener('resize', getWindowWidth);
-
-    return () => {
-      window.removeEventListener('resize', getWindowWidth);
-    };
-  }, []);
-
-  const getWindowWidth = () => {
-    setWindowWidth(window.innerWidth);
-  };
 
   const snapTo = (index) => {
-    sheetRef.current.snapTo(index);
+    sheetRef.current?.snapTo(index);
   };
 
   return (
@@ -36,34 +22,34 @@ export default function ChordButtonsSection() {
           <ChordList />
         </Container>
       </section>
-      {windowWidth < lgBreakpoint && (
-        <Sheet
-          ref={sheetRef}
-          isOpen={true}
-          onClose={() => snapTo(2)}
-          snapPoints={snapPoints}
-          initialSnap={initialSnapIndex}
-          springConfig={springConfig}
-        >
-          <Sheet.Container>
-            <Sheet.Header>
-              <span className="sheet-header">
-                Choose one of the chord types below
-              </span>
-            </Sheet.Header>
-            <Sheet.Content
-              disableDrag
-              style={{
-                paddingBottom: sheetRef.current && sheetRef.current.y.current,
-              }}
-            >
-              <Container className="pt-4 pb-10">
-                <ChordList snapTo={snapTo} />
-              </Container>
-            </Sheet.Content>
-          </Sheet.Container>
-        </Sheet>
-      )}
+
+      <Sheet
+        ref={sheetRef}
+        isOpen={true}
+        onClose={() => snapTo(2)}
+        snapPoints={snapPoints}
+        initialSnap={initialSnapIndex}
+        springConfig={springConfig}
+        className="lg:hidden"
+      >
+        <Sheet.Container>
+          <Sheet.Header>
+            <span className="sheet-header">
+              Choose one of the chord types below
+            </span>
+          </Sheet.Header>
+          <Sheet.Content
+            disableDrag
+            style={{
+              paddingBottom: sheetRef.current?.y,
+            }}
+          >
+            <Container className="pt-4 pb-10">
+              <ChordList snapTo={snapTo} />
+            </Container>
+          </Sheet.Content>
+        </Sheet.Container>
+      </Sheet>
     </>
   );
 }
